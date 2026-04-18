56a31e26247571279c8e22978ee75c12b5d8ff4b
Polijen <victor4rushwars@gmail.com>
2026-04-18 15:10:13 +0300

    Add files via upload

---
+++ Teme_Lab/Tema_2.cpp
@@ -0,0 +1,83 @@
+#include <fcntl.h>
+#include <io.h>
+#include <Windows.h>
+#include <iostream>
+#include <vector>
+#include <iomanip>
+	
+	//Tema 2: Sa se proiecteze o aplicatie (PowerShell, cmd, MSVC/C++ preferabil) care sa identifice toate serviciile care ruleaza la modul curent pe masina
+	//Folosesc EnumServices pentru a obtine lista tuturor serviciilor, apoi EnumDependentServices pentru a verifica daca sunt in starea de Running
+	
+	//Optional: Sa se identifice dll-urile multiple
+	
+int main() {
+	
+	//FIX: pentru caracterele Unicode, sa se afiseze corect 
+	_setmode(_fileno(stdout), _O_U16TEXT);
+
+	//Deschidem Service Control Manager pentru a putea enumera serviciile
+	SC_HANDLE hSCManager = OpenSCManagerW(NULL, NULL, SC_MANAGER_ENUMERATE_SERVICE); //NULL pentru serverul local, NULL pentru grupul de servicii (toate), iar ultimul argument specifica permisiunea necesara (doar sa citim lista)
+	if (!hSCManager) {
+		std::wcerr << L"Failed to open Service Control Manager. Error: " << GetLastError() << std::endl;
+		return 1;
+	}
+
+
+	DWORD dwBytesNeeded = 0;
+	DWORD dwServiceCount = 0;
+	DWORD dwResumeHandle = 0;
+
+	//Prima apelare pentru a afla dimensiunea necesara a bufferului
+	EnumServicesStatusExW(
+		hSCManager,
+		SC_ENUM_PROCESS_INFO, //Obtin informatii despre procesele asociate serviciilor
+		SERVICE_WIN32, //Enumerez doar serviciile de tip Win32
+		SERVICE_STATE_ALL, //Enumerez toate serviciile indiferent de starea lor
+		NULL, 0, //Bufferul pentru rezultate va fi alocat dupa ce stim dimensiunea necesara
+		&dwBytesNeeded,
+		&dwServiceCount,
+		&dwResumeHandle,
+		NULL
+	);
+
+	//alocam bufferul necesar pentru a stoca informatiile despre servicii pe baza output-ului din prima apelare
+	std::vector<BYTE> buffer(dwBytesNeeded); //Alocam bufferul necesar
+	LPENUM_SERVICE_STATUS_PROCESSW services = reinterpret_cast<LPENUM_SERVICE_STATUS_PROCESSW>(buffer.data());
+
+	if (!EnumServicesStatusExW(
+		hSCManager,
+		SC_ENUM_PROCESS_INFO,
+		SERVICE_WIN32,
+		SERVICE_STATE_ALL,
+		buffer.data(),
+		dwBytesNeeded,
+		&dwBytesNeeded,
+		&dwServiceCount,
+		&dwResumeHandle,
+		NULL
+	)) {
+		std::wcerr << L"Failed to enumerate services. Error: " << GetLastError() << std::endl;
+		CloseServiceHandle(hSCManager);
+		return 1;
+	}
+	else {
+		std::wcout << L"Servicii care ruleaza in prezent:\n";
+		std::wcout << L"-----------------------------------\n";	
+
+		int runningServicesCount = 0;
+
+		for (DWORD i = 0; i < dwServiceCount; i++) {
+			//Verifica daca serviciul curent este in starea de Running
+			if (services[i].ServiceStatusProcess.dwCurrentState == SERVICE_RUNNING) {
+				std::wcout << L"[RUNNING] " << services[i].lpServiceName << L" - " << services[i].lpDisplayName << std::endl;
+				runningServicesCount++;
+			}
+		}
+
+	}
+
+	//Cleaning
+	CloseServiceHandle(hSCManager);
+
+	return 0;
+}
